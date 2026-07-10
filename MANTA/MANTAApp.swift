2d0590485f246aff656733185af8204f4b2ac781
Peter Molfese <pmolfese@gmail.com>
2026-07-10 14:31:30 -0400

    Initial Commit

---
+++ MANTA/MANTAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MANTAApp.swift
+//  MANTA
+//
+//  Created by Molfese, Peter  [E] on 7/10/26.
+//
+
+import SwiftUI
+
+@main
+struct MANTAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MANTATests/MANTATests.swift
@@ -0,0 +1,19 @@
+//
+//  MANTATests.swift
+//  MANTATests
+//
+//  Created by Molfese, Peter  [E] on 7/10/26.
+//
+
+import Testing
+@testable import MANTA
+
+struct MANTATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
