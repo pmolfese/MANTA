c159f39bed0e01b201f37da049061129ea23765e
Peter Molfese <pmolfese@gmail.com>
2026-07-13 00:45:04 -0400

    icons

--- MANTAReceiver/MANTAReceiverApp.swift
+++ MANTAReceiver/MANTAReceiverApp.swift
@@ -1,12 +1,20 @@
 import AppKit
 import SwiftUI
 
+private enum MANTAReceiverBranding {
+    static let applicationName = "MANTA Receiver"
+
+    static let applicationIcon: NSImage? = {
+        guard let iconURL = Bundle.main.url(forResource: "AppIcon", withExtension: "icns") else {
+            return nil
+        }
+        return NSImage(contentsOf: iconURL)
+    }()
+}
+
 final class MANTAReceiverAppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillFinishLaunching(_ notification: Notification) {
-        guard
-            let iconURL = Bundle.main.url(forResource: "AppIcon", withExtension: "icns"),
-            let icon = NSImage(contentsOf: iconURL)
-        else {
+        guard let icon = MANTAReceiverBranding.applicationIcon else {
             return
         }
 
@@ -22,6 +30,19 @@ struct MANTAReceiverApp: App {
         WindowGroup("MANTA Receiver") {
             ReceiverContentView()
                 .frame(minWidth: 900, minHeight: 600)
+        }
+        .commands {
+            CommandGroup(replacing: .appInfo) {
+                Button("About \(MANTAReceiverBranding.applicationName)") {
+                    var options: [NSApplication.AboutPanelOptionKey: Any] = [
+                        .applicationName: MANTAReceiverBranding.applicationName
+                    ]
+                    if let icon = MANTAReceiverBranding.applicationIcon {
+                        options[.applicationIcon] = icon
+                    }
+                    NSApplication.shared.orderFrontStandardAboutPanel(options: options)
+                }
+            }
         }
     }
 }
