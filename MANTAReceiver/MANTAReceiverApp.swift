d6f3a08060cdc2f10fe528a35690baa1d6e16748
Peter Molfese <pmolfese@gmail.com>
2026-07-13 00:43:45 -0400

    icon

--- MANTAReceiver/MANTAReceiverApp.swift
+++ MANTAReceiver/MANTAReceiverApp.swift
@@ -1,7 +1,23 @@
+import AppKit
 import SwiftUI
+
+final class MANTAReceiverAppDelegate: NSObject, NSApplicationDelegate {
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        guard
+            let iconURL = Bundle.main.url(forResource: "AppIcon", withExtension: "icns"),
+            let icon = NSImage(contentsOf: iconURL)
+        else {
+            return
+        }
+
+        NSApplication.shared.applicationIconImage = icon
+    }
+}
 
 @main
 struct MANTAReceiverApp: App {
+    @NSApplicationDelegateAdaptor(MANTAReceiverAppDelegate.self) private var appDelegate
+
     var body: some Scene {
         WindowGroup("MANTA Receiver") {
             ReceiverContentView()
